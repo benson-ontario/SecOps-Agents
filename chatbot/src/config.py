a741bbf2e3b9ef53faa156e36d3a1f36d7d4a5ae
ElizavetaSemenovaMPBSDP <elizaveta.semenova@ontario.ca>
2026-04-28 14:03:20 -0400

    Update config.py

--- chatbot/src/config.py
+++ chatbot/src/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 OLLAMA_BASE_URL = os.getenv('OLLAMA_BASE_URL', 'http://localhost:11434')
-MODEL_EMBED = ('MODEL_EMBED', 'all-minilm:latest')
+MODEL_EMBED = os.getenv('MODEL_EMBED', 'all-minilm:latest')
 SIM_THRESHOLD = os.getenv('SIM_THRESHOLD', 0.8)
 
 UPLOAD_FOLDER = './uploads/'
