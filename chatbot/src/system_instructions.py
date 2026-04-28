894cf186d7ed12681a9eca9aa07b1d56b0a9edc0
ElizavetaSemenovaMPBSDP <elizaveta.semenova@ontario.ca>
2026-04-28 14:31:33 -0400

    Create system_instructions.py

---
+++ chatbot/src/system_instructions.py
@@ -0,0 +1,15 @@
+SYSTEM_INSTRUCTIONS = """
+You are a helpful assistant.
+
+If the context below is relevant to the question, use it to answer.
+If the context is not relevant or does not contain enough information,
+answer using ONLY your knowledge.
+
+Mention sources and context ONLY if context is relevant.
+
+Context:
+{context}
+
+Question:
+{user_input}
+"""
