353b8f49a89bece5b080eafb27b5304a2150a2bd
ElizavetaSemenovaMPBSDP <elizaveta.semenova@ontario.ca>
2026-05-01 10:44:58 -0400

    Update async_agent.py

--- bedrock_agent/mcp_server/async_agent.py
+++ bedrock_agent/mcp_server/async_agent.py
@@ -134,7 +134,7 @@ async def agent(request: Request, body: PromptRequest):
 
 if __name__=="__main__":
     uvicorn.run(
-        "test_agent:app",
+        "async_agent:app",
         host="0.0.0.0",
         port=8000,
         reload=True
